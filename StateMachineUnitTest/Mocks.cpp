2254bf5846d06ce099b169c44af243a60ced02e3
cozzyy2002 <cozzyy2002@gmail.com>
2018-02-03 22:01:17 +0900

    Modify MockEvent/MockState to check reference count.

--- StateMachineUnitTest/Mocks.cpp
+++ StateMachineUnitTest/Mocks.cpp
@@ -2,6 +2,13 @@
 
 #include "Mocks.h"
 
+TestUnknown::~TestUnknown()
+{
+	if(rcRef != 0) {
+		ADD_FAILURE() << className << ": Reference count is not zero at destruction: " << (LONG)rcRef;
+	}
+}
+
 bool TestUnknown::deleted() const
 {
 	return (rcRef == 0);
@@ -12,13 +19,18 @@ bool TestUnknown::deleted() const
  *
  * This method does not delete this object even if reference count reaches zero to:
  *   Be created in stack.
- *   check where the object is deleted by calling deleted() method.
+ *   check whether the object is deleted by calling deleted() method.
  */
-ULONG TestUnknown::Release(IUnknown* _this)
+ULONG TestUnknown::Release()
 {
 	rcRef = InterlockedDecrement(&rcRef);
 	if((LONG)rcRef < 0) {
-		ADD_FAILURE() << typeid(*_this).name() << ": Invalid reference count: " << (LONG)rcRef;
+		ADD_FAILURE() << className << ": Invalid reference count: " << (LONG)rcRef;
 	}
 	return rcRef;
+}
+
+void TestUnknown::setObject(IUnknown * _this)
+{
+	className = typeid(*_this).name();
 }
--- StateMachineUnitTest/Mocks.h
+++ StateMachineUnitTest/Mocks.h
@@ -18,29 +18,34 @@ class TestUnknown
 {
 public:
 	TestUnknown(ULONG& cRef) : rcRef(cRef) {}
+	virtual ~TestUnknown();
 
 	bool deleted() const;
-	ULONG Release(IUnknown* _this);
+	ULONG Release();
 
 protected:
+	// Initialize className using _this pointer.
+	void setObject(IUnknown* _this);
+
 	ULONG& rcRef;
+	std::string className;
 };
 
 class MockEvent : public tsm::Event, public TestUnknown
 {
 public:
-	MockEvent() : TestUnknown(m_cRef) {}
+	MockEvent() : TestUnknown(m_cRef) { setObject(this); }
 
-	virtual ULONG STDMETHODCALLTYPE Release(void) { return TestUnknown::Release(this); }
+	virtual ULONG STDMETHODCALLTYPE Release(void) { return TestUnknown::Release(); }
 };
 
 class MockState : public tsm::State<MockContext, MockEvent, MockState>, public TestUnknown
 {
 public:
-	MockState() : TestUnknown(m_cRef) {}
+	MockState() : TestUnknown(m_cRef) { setObject(this); }
 
 	void setMasterState(MockState* masterState) { m_masterState = masterState; }
-	virtual ULONG STDMETHODCALLTYPE Release(void) { return TestUnknown::Release(this); }
+	virtual ULONG STDMETHODCALLTYPE Release(void) { return TestUnknown::Release(); }
 
 	MOCK_METHOD3(handleEvent, HRESULT(MockContext*, MockEvent*, MockState**));
 	MOCK_METHOD3(entry, HRESULT(MockContext*, MockEvent*, MockState*));
