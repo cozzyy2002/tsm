6b5742e8717a5800ec3be6d592b8d5cba534a1da
cozzyy2002 <cozzyy2002@gmail.com>
2018-04-15 16:00:25 +0900

    Move IStateMonitor implementation from MyLogger to MyContext.

--- StateMachineManualTest/MyContext.cpp
+++ StateMachineManualTest/MyContext.cpp
@@ -3,17 +3,55 @@
 #include <StateMachine/Assert.h>
 #include "MyContext.h"
 
+static log4cplus::Logger logger = log4cplus::Logger::getInstance(_T("MyContext"));
+
 MyContext::MyContext()
 {
-	tsm::IContext::onAssertFailedProc = MyLogger::onAssertFailed;
-}
-
-tsm::IStateMonitor* MyContext::_getStateMonitor()
-{
-	return &m_logger;
+	tsm::IContext::onAssertFailedProc = MyContext::onAssertFailed;
 }
 
 void MyContext::createStateMachine(HWND hWnd, UINT msg)
 {
 	m_stateMachine.reset(tsm::IStateMachine::create(hWnd, msg));
+}
+
+
+template<class T>
+LPCTSTR MyContext::toString(T* obj)
+{
+	auto _obj = dynamic_cast<MyObject*>(obj);
+	return _obj ? _obj->toString() : _T("<nullptr>");
+}
+
+void MyContext::onIdle(tsm::IContext* context)
+{
+	LOG4CPLUS_INFO(logger, "StateMachined is idle.");
+}
+
+void MyContext::onEventTriggered(tsm::IContext* context, tsm::IEvent* event)
+{
+}
+
+void MyContext::onEventHandling(tsm::IContext* context, tsm::IEvent* event, tsm::IState* current)
+{
+	LOG4CPLUS_INFO(logger, "Handling event " << toString(event) << " in state " << toString(current));
+}
+
+void MyContext::onStateChanged(tsm::IContext* context, tsm::IEvent* event, tsm::IState* previous, tsm::IState* next)
+{
+	LOG4CPLUS_INFO(logger, "State changed from " << toString(previous) << " to " << toString(next));
+}
+
+void MyContext::onTimerStarted(tsm::IContext* context, tsm::IEvent* event)
+{
+}
+
+void MyContext::onWorkerThreadExit(tsm::IContext* context, HRESULT exitCode)
+{
+	LOG4CPLUS_INFO(logger, "Worker thread exit. Code=0x" << std::hex << exitCode);
+}
+
+void MyContext::onAssertFailed(HRESULT hr, LPCTSTR exp, LPCTSTR sourceFile, int line)
+{
+	LOG4CPLUS_ERROR(logger, exp << " failed. HRESULT=0x" << std::hex << hr << ". at:\n" << sourceFile << "#L" << std::dec << line);
 }
--- StateMachineManualTest/MyContext.h
+++ StateMachineManualTest/MyContext.h
@@ -2,19 +2,29 @@
 
 #include <StateMachine/Context.h>
 #include "MyObject.h"
-#include "MyLogger.h"
 
 class MyState;
 class MyEvent;
 
-class MyContext : public tsm::AsyncContext<MyEvent, MyState>, public MyObject
+class MyContext : public tsm::AsyncContext<MyEvent, MyState>, tsm::IStateMonitor, public MyObject
 {
 public:
 	MyContext();
 
-	virtual tsm::IStateMonitor* _getStateMonitor() override;
+	virtual tsm::IStateMonitor* _getStateMonitor() override { return this; }
 	void createStateMachine(HWND hWnd, UINT msg);
 
-protected:
-	MyLogger m_logger;
+#pragma region Implementation of IStateMonitor
+	virtual void onIdle(tsm::IContext* context) override;
+	virtual void onEventTriggered(tsm::IContext* context, tsm::IEvent* event) override;
+	virtual void onEventHandling(tsm::IContext* context, tsm::IEvent* event, tsm::IState* current) override;
+	virtual void onStateChanged(tsm::IContext* context, tsm::IEvent* event, tsm::IState* previous, tsm::IState* next) override;
+	virtual void onTimerStarted(tsm::IContext* context, tsm::IEvent* event) override;
+	virtual void onWorkerThreadExit(tsm::IContext* context, HRESULT exitCode) override;
+#pragma endregion
+
+	static void onAssertFailed(HRESULT hr, LPCTSTR exp, LPCTSTR sourceFile, int line);
+
+	template<class T>
+	static LPCTSTR toString(T* obj);
 };
