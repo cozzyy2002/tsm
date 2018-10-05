8b94d4caea9cf6c4bbd7712d22fccd98942140ef
cozzyy2002 <cozzyy2002@gmail.com>
2018-10-05 21:26:04 +0900

    Modify to log HRESULT value returned by state handler methods

--- StateMachineManualTest/MyState.cpp
+++ StateMachineManualTest/MyState.cpp
@@ -14,7 +14,7 @@ ULONG MyState::Release(void)
 
 HRESULT MyState::handleEvent(MyContext*, MyEvent* event, MyState** nextState)
 {
-	m_logger->log(_T("%s::handleEvent()"), MyObject::toString());
+	m_logger->log(_T("%s::handleEvent(): HRESULT=0x%p"), MyObject::toString(), event->hrHandleEvent);
 
 	*nextState = event->nextState;
 	return event->hrHandleEvent;
@@ -22,15 +22,17 @@ HRESULT MyState::handleEvent(MyContext*, MyEvent* event, MyState** nextState)
 
 HRESULT MyState::entry(MyContext* context, MyEvent* event, MyState* previousState)
 {
-	m_logger->log(_T("%s::entry()"), MyObject::toString());
-
 	// event might be nullptr when entry() is called from Context::setup().
-	return event ? event->hrEntry : S_OK;
+	auto hr = event ? event->hrEntry : S_OK;
+
+	m_logger->log(_T("%s::entry(): HRESULT=0x%p"), MyObject::toString(), hr);
+	return hr;
 }
 
 HRESULT MyState::exit(MyContext* context, MyEvent* event, MyState* nextState)
 {
-	m_logger->log(_T("%s::exit()"), MyObject::toString());
+	auto hr = event ? event->hrExit : S_OK;
 
-	return event ? event->hrExit : S_OK;
+	m_logger->log(_T("%s::exit(): HRESULT=0x%p"), MyObject::toString(), hr);
+	return hr;
 }
