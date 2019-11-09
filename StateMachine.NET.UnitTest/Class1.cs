bb1cd432f0800d6b4d2bd8435cff4257daa478ba
Koji Yano <cozzyy2002@gmail.com>
2019-11-09 21:32:49 +0900

    Add unit test for StateMachine.NET.  * Add NUnit3 package using NuGet.  * Modifiy StateMahine.NET project configuration for build x64.

---
+++ StateMachine.NET.UnitTest/Class1.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace StateMachine.NET.UnitTest
+{
+    public class Context : tsm_NET.Generic.Context<Event, State>
+    {
+    }
+
+    public class Event : tsm_NET.Generic.Event<Context>
+    {
+
+    }
+
+    public class State : tsm_NET.Generic.State<Context, Event, State>
+    {
+
+    }
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test1()
+        {
+            var c = new Context();
+            Assert.That(c.CurrentState, Is.EqualTo(null), "error");
+        }
+    }
+}
