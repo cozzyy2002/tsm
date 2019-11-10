c60b6f293c14083a74df4e781a9ec6e0ba8bc7ca
Koji Yano <cozzyy2002@gmail.com>
2019-11-10 18:31:15 +0900

    Add NSubstitute package using NuGet.  * FIXIT: NSubstitute can not detect error that the mock method is not called.

--- StateMachine.NET.UnitTest/Class1.cs
+++ StateMachine.NET.UnitTest/Class1.cs
@@ -1,7 +1,12 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Threading;
 
-namespace StateMachine.NET.UnitTest
+namespace StateMachine.NET.UnitTest.Generic
 {
+    using HResult = tsm_NET.HResult;
+
     public class Context : tsm_NET.Generic.Context<Event, State>
     {
     }
@@ -13,17 +18,56 @@ namespace StateMachine.NET.UnitTest
 
     public class State : tsm_NET.Generic.State<Context, Event, State>
     {
-
+        public override HResult handleEvent(Context context, Event @event, ref State nextState)
+        {
+            Console.WriteLine($"{this.GetType().ToString()}.handleEvent() is called.");
+            Assert.That(context, Is.TypeOf<Context>());
+            return HResult.Ok;
+        }
     }
 
     [TestFixture]
     public class Class1
     {
+        [TearDown]
+        public void TearDown()
+        {
+            // Wait for worker thread of StateMachine to process events and to terminate.
+            //Thread.Sleep(1000);
+        }
+
         [Test]
         public void test1()
         {
+            var mockState = Substitute.For<State>();
+
             var c = new Context();
-            Assert.That(c.CurrentState, Is.EqualTo(null), "error");
+            Assert.That(c.CurrentState, Is.EqualTo(null), "Context has no initial state when created.");
+
+            mockState
+                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>())
+                .Returns(x =>
+                {
+                    Console.Error.WriteLine($"{this.GetType().ToString()}.entry() is called.");
+                    return Enum.ToObject(typeof(HResult), 0xc0000002);
+                });
+            mockState
+                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>())
+                .Returns(x =>
+                {
+                    Console.Error.WriteLine($"{this.GetType().ToString()}.handleEvent() is called.");
+                    return tsm_NET.HResult.Ok;
+                });
+
+            Assert.That(c.setup(mockState), Is.EqualTo(HResult.Ok));
+            Assert.That(c.CurrentState, Is.EqualTo(mockState), "Context has argument of setup() as initial state.");
+
+            Thread.Sleep(1000);
+
+            mockState.Received()
+                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>());
+            mockState.Received()
+                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>());
         }
     }
 }
---
+++ StateMachine.NET.UnitTest/Class2.cs
@@ -0,0 +1,33 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using tsm_NET;
+
+namespace StateMachine.NET.UnitTest
+{
+    class Class2
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            // Wait for worker thread of StateMachine to process events and to terminate.
+            Thread.Sleep(100);
+        }
+
+        [Test]
+        public void test1()
+        {
+            var mockState = Substitute.For<State>();
+            mockState
+                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>())
+                .Returns(x =>
+                {
+                    return HResult.Ok;
+                });
+            var c = new Context();
+            Assert.That(c.CurrentState, Is.EqualTo(null), "error");
+
+        }
+    }
+}
