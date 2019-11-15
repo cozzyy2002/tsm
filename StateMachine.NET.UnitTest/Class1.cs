0535ecec853c24d71b6678f135eb46d54d89e9b2
Koji Yano <cozzyy2002@gmail.com>
2019-11-15 22:05:31 +0900

    Modify unit test case for generic classes.

--- StateMachine.NET.UnitTest/Class1.cs
+++ StateMachine.NET.UnitTest/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using NSubstitute;
 using NUnit.Framework;
 using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace StateMachine.NET.UnitTest.Generic
@@ -13,7 +14,7 @@ namespace StateMachine.NET.UnitTest.Generic
 
     public class Event : tsm_NET.Generic.Event<Context>
     {
-
+        public static Event Null = null;
     }
 
     public class State : tsm_NET.Generic.State<Context, Event, State>
@@ -24,6 +25,8 @@ namespace StateMachine.NET.UnitTest.Generic
             Assert.That(context, Is.TypeOf<Context>());
             return HResult.Ok;
         }
+
+        public static State Null = null;
     }
 
     [TestFixture]
@@ -45,54 +48,28 @@ namespace StateMachine.NET.UnitTest.Generic
             Assert.That(c.CurrentState, Is.EqualTo(null), "Context has no initial state when created.");
 
             mockState
-                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>())
+                .entry(Arg.Is<Context>(c), Arg.Is<Event>(x => x == null), Arg.Is<State>(x => x == null))
                 .Returns(x =>
                 {
-                    Console.Error.WriteLine($"{this.GetType().ToString()}.handleEvent() is called.");
+                    Trace.WriteLine($"{mockState.GetType()}.entry({x[0]}) is called.");
                     return tsm_NET.HResult.Ok;
                 });
 
             Assert.That(c.setup(mockState), Is.EqualTo(HResult.Ok));
             Assert.That(c.CurrentState, Is.EqualTo(mockState), "Context has argument of setup() as initial state.");
 
+            var e = Substitute.For<Event>();
+            Assert.That(c.triggerEvent(e), Is.EqualTo(HResult.Ok));
+
             Thread.Sleep(1000);
 
-            mockState.Received()
-                .entry(Arg.Any<Context>(), Arg.Any<Event>(), Arg.Any<State>());
-            mockState.DidNotReceive()
-                .handleEvent(Arg.Any<Context>(), Arg.Any<Event>(), ref Arg.Any<State>());
-        }
-    }
-
-    public interface ICommand
-    {
-        void Execute();
-        event EventHandler Executed;
-    }
-
-    public class SomethingThatNeedsACommand
-    {
-        ICommand command;
-        public SomethingThatNeedsACommand(ICommand command)
-        {
-            this.command = command;
-        }
-        public void DoSomething() { command.Execute(); }
-        public void DontDoAnything() { }
-    }
-
-    public class Sample
-    {
-        [Test]
-        public void Should_execute_command()
-        {
-            //Arrange
-            var command = Substitute.For<ICommand>();
-            var something = new SomethingThatNeedsACommand(command);
-            //Act
-            something.DoSomething();
-            //Assert
-            command.Received().Execute();
+            Received.InOrder(() =>
+            {
+                mockState.Received()
+                    .entry(Arg.Is(c), Arg.Is(Event.Null), Arg.Is(State.Null));
+                mockState.Received()
+                    .handleEvent(Arg.Is(c), Arg.Is(e), ref Arg.Is(State.Null));
+            });
         }
     }
 }
