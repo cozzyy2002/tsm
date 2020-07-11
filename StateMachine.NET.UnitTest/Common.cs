2b843816f60c300f0b173a0f4b1dc0366576882d
koji.sy.yano <koji.sy.yano@jp.ricoh.com>
2020-07-11 17:54:03 +0900

    Add StateMachine.NET.UniTest(Now implementing)

---
+++ StateMachine.NET.UnitTest/Common.cs
@@ -0,0 +1,20 @@
+﻿namespace StateMachine.NET.UnitTest.Common
+{
+    namespace Async
+    {
+        public class Context : tsm_NET.Generic.AsyncContext<Event, State>
+        {
+            public Context() : base(true) { }
+        }
+
+        public class Event : tsm_NET.Generic.Event<Context>
+        {
+            public static Event Null { get { return null; } }
+        }
+
+        public class State : tsm_NET.Generic.State<Context, Event, State>
+        {
+            public static State Null { get { return null; } }
+        }
+    }
+}
---
+++ StateMachine.NET.UnitTest/StateMachine.NET.UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using StateMachine.NET.UnitTest.Common.Async;
+using System;
+using tsm_NET.Generic;
+
+namespace StateMachine.NET.UnitTest.Generic
+{
+    [TestFixture]
+    class StateMachineSetupUnitTest
+    {
+        Context context;
+        State state;
+
+        [SetUp]
+        public void SetUp()
+        {
+            context = new Context();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Assert.That(context.CurrentState, Is.EqualTo(State.Null));
+        }
+
+        // StateMachine.setup(Event = null)
+        [Test]
+        public void SetupUnitTest_0()
+        {
+            var mockState = Substitute.For<State>();
+
+            Assert.That(context.setup(mockState), Is.EqualTo(HResult.Ok));
+            Assert.That(context.waitReady(TimeSpan.FromSeconds(1)), Is.EqualTo(HResult.Ok));
+
+            mockState.Received()
+                .entry(Arg.Is(context), Arg.Is(Event.Null), Arg.Is(State.Null));
+
+            Assume.That(context.CurrentState, Is.EqualTo(mockState));
+            Assert.That(context.shutdown(), Is.EqualTo(HResult.Ok));
+        }
+
+        // StateMachine.setup(Event = event)
+        [Test]
+        public void SetupUnitTest_1()
+        {
+            var mockEvent = Substitute.For<Event>();
+            var mockState = Substitute.For<State>();
+
+            Assert.That(context.setup(mockState, mockEvent), Is.EqualTo(HResult.Ok));
+            Assert.That(context.waitReady(TimeSpan.FromSeconds(1)), Is.EqualTo(HResult.Ok));
+
+            mockState.Received()
+                .entry(Arg.Is(context), Arg.Is(mockEvent), Arg.Is(State.Null));
+
+            Assume.That(context.CurrentState, Is.EqualTo(mockState));
+            Assert.That(context.shutdown(), Is.EqualTo(HResult.Ok));
+        }
+    }
+}
