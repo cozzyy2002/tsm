99e9472c432950ed096a3b9ab2d4527d1b014ef7
Koji Yano <cozzyy2002@gmail.com>
2020-08-26 23:20:59 +0900

    Add unite test case for One-Shot timer.

---
+++ StateMachine.NET.UnitTest/StateMachine.NET.TimerUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StateMachine.NET.TimerUnitTest
+{
+    using Context = Testee.Generic.AsyncContext;
+    using Event = Testee.Generic.AsyncEvent;
+    using State = Testee.Generic.AsyncState;
+    using HResult = tsm_NET.Generic.HResult;
+
+    [TestFixture]
+    class StateMachineTimerUnitTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            context = new Context();
+            mockState0 = Substitute.For<State>();
+            mockState1 = Substitute.For<State>();
+            e0 = new Event();
+            e1 = new Event();
+            mockState0.IsExitCalledOnShutdown = true;
+            Assert.That(context.setup(mockState0), Is.EqualTo(HResult.Ok));
+            Assert.That(context.waitReady(TimeSpan.FromSeconds(1)), Is.EqualTo(HResult.Ok));
+            mockState0.Received().entry(context, null, null);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Assert.That(context.shutdown(TimeSpan.FromSeconds(1)), Is.EqualTo(HResult.Ok));
+            mockState0.Received().exit(context, null, null);
+        }
+
+        protected Context context;
+        protected Event e0, e1;
+        protected State mockState0, mockState1;
+    }
+
+    [TestFixture(true)]     // Use Context as TimerClient
+    [TestFixture(false)]    // Use State as TimerClient
+    class StateMachineTimerClientUnitTest : StateMachineTimerUnitTest
+    {
+        public StateMachineTimerClientUnitTest(bool isTimerClientContext)
+        {
+            this.isTimerClientContext = isTimerClientContext;
+        }
+
+        [SetUp]
+        public void SetUpTimerClient()
+        {
+            // Set timerClient according to argument of TestFixture attribute.
+            timerClient = isTimerClientContext ? (tsm_NET.TimerClient)context : mockState0;
+        }
+
+        bool isTimerClientContext;
+        tsm_NET.TimerClient timerClient;
+
+        // One-Shot timer
+        [Test]
+        public void OneShotTimer()
+        {
+            Console.WriteLine($"One-Shot timer test using {timerClient}");
+
+            e0.setDelayTimer(timerClient, TimeSpan.FromMilliseconds(100));
+            Assert.That(context.triggerEvent(e0), Is.EqualTo(HResult.Ok));
+            Thread.Sleep(50);
+            var ev = timerClient.PendingEvents;
+            Assert.That(ev.Count, Is.EqualTo(1));
+            Assert.That(ev.Contains(e0), Is.True);
+            Thread.Sleep(100);
+            ev = timerClient.PendingEvents;
+            Assert.That(ev.Count, Is.EqualTo(0));
+
+            mockState0.Received()
+                .handleEvent(context, e0, ref Arg.Any<State>());
+        }
+    }
+}
