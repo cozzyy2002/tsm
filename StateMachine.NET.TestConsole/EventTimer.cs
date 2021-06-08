f386d52b6a0a892ceeead4bf764f9ab0a70ae8ae
Koji Yano <cozzyy2002@gmail.com>
2021-06-08 23:05:28 +0900

    Fix StateMonitor of StateMachine.NET.TestConsole application.

--- StateMachine.NET.TestConsole/EventTimer.cs
+++ StateMachine.NET.TestConsole/EventTimer.cs
@@ -11,7 +11,7 @@ namespace StateMachine.NET.TestConsole
         void IJob.Start(IList<string> args)
         {
             var context = new Context();
-            context.StateMonitor = new StateMonitor<IContext, IEvent, IState>();
+            context.StateMonitor = new StateMonitor();
 
             context.Count = 2;
             var initialState = new InitialState();
@@ -99,37 +99,37 @@ namespace StateMachine.NET.TestConsole
                 Console.WriteLine($"{Now} [{Context.CurrentThread}] {caller}({args})");
             }
 
-            public void onEventHandling(Context<Event, State> context, Event @event, State current)
+            public override void onEventHandling(Context context, Event @event, State current)
             {
                 print($"{context}, {@event}, {current}");
             }
 
-            public void onEventTriggered(Context<Event, State> context, Event @event)
+            public override void onEventTriggered(Context context, Event @event)
             {
                 print($"{context}, {@event}");
             }
 
-            public void onIdle(Context<Event, State> context)
+            public override void onIdle(Context context)
             {
                 print($"{context}");
             }
 
-            public void onStateChanged(Context<Event, State> context, Event @event, State previous, State next)
+            public override void onStateChanged(Context context, Event @event, State previous, State next)
             {
                 print($"{context}, {@event}, {previous}, {next}");
             }
 
-            public void onTimerStarted(Context<Event, State> context, Event @event)
+            public override void onTimerStarted(Context context, Event @event)
             {
                 print($"{context}, {@event}");
             }
 
-            public void onTimerStopped(Context<Event, State> context, Event @event, HResult hr)
+            public override void onTimerStopped(Context context, Event @event, HResult hr)
             {
                 print($"{context}, {@event}, {hr,08:x}");
             }
 
-            public void onWorkerThreadExit(Context<Event, State> context, HResult exitCode)
+            public override void onWorkerThreadExit(Context context, HResult exitCode)
             {
                 print($"{context}, ExitCode={exitCode}");
             }
